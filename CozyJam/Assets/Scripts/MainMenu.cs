8a8f8d91e04636cd32d8131db2c00b6ca587e7bc
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2026-02-26 11:39:31 +0000

    bug fix main menu

--- CozyJam/Assets/Scripts/MainMenu.cs
+++ CozyJam/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        //TODO JUMP TO SCENE 
+        SceneManager.LoadScene(1);
     }
 
     public void Exit()
